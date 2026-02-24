f2ea6547da27d6ba38981763224365172b85eb08
Oliver Schafeld <oliver@schafeld.com>
2026-02-24 08:48:54 +0100

    add About page

--- next-demo-app/app/about/page.js
+++ next-demo-app/app/about/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">About Page</h1>
+      <Link href="/" className="text-blue-500 underline">
+        Go back to Home Page
+      </Link>
     </main>
   );
 }
--- next-demo-app/app/page.tsx
+++ next-demo-app/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Link href="/about" className="text-blue-500 underline">
+        Go to About Page
+      </Link>
     </main>
   );
 }
