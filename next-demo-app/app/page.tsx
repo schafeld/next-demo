c61ec906a0eb124c114df507a48570049955cb01
Oliver Schafeld <oliver@schafeld.com>
2026-02-24 14:37:14 +0100

    add .env dummy

--- next-demo-app/app/page.tsx
+++ next-demo-app/app/page.tsx
@@ -19,6 +19,8 @@ export default function Home() {
         <li><Link href="/posts/2" className="text-blue-500 underline">Post 2</Link></li>
         <li><Link href="/posts/3" className="text-blue-500 underline">Post 3</Link></li>
       </ol>
+
+      <p>Oh, guck, ein Key, hihi: <em>{process.env.TOTALLY_MADE_UP_PSEUDO_KEY}</em></p>
     </Layout>
   );
 }
