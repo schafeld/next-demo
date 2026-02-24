a7fa84c0a1b6053467ae89938f9f2244f745ec7f
Oliver Schafeld <oliver@schafeld.com>
2026-02-24 10:09:05 +0100

    get posts data from json mock

--- next-demo-app/app/posts/[id]/page.js
+++ next-demo-app/app/posts/[id]/page.js
@@ -1,12 +1,29 @@
 import Link from "next/link";
+import { posts } from "../../data/posts";
 
 export default async function Post({ params }) {
     const { id } = await params;
+    const post = posts.find((p) => p.id === parseInt(id));
+
+    if (!post) {
+        return (
+            <main>
+                <h1 className="text-3xl font-bold underline">Post Not Found</h1>
+                <p>The post you are looking for does not exist.</p>
+                <p>
+                    <Link href="/" className="text-blue-500 underline">
+                        Go back to Home Page
+                    </Link>
+                </p>
+            </main>
+        );
+    }
 
     return (
         <main>
-            <h1 className="text-3xl font-bold underline">Post Id: {id}</h1>
-            
+            <h1 className="text-3xl font-bold underline">{post.title}</h1>
+            <p>{post.content}</p>
+
             <p>
                 <Link href="/" className="text-blue-500 underline">
                     Go back to Home Page
